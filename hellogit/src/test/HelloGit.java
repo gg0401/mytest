ae49eb3d033ff13896a10c83a383d6d65eca4616
gg <gg@126.com>
2017-09-01 22:21:08 +0800

    主线程上修改

--- hellogit/src/test/HelloGit.java
+++ hellogit/src/test/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("主线程上修改");
 		System.out.println("hello git.....");
 	}
 
