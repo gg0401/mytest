1712065bb96e11abb006b6899eb0ffbb1eede826
gg <gg@126.com>
2017-09-01 22:18:34 +0800

    分支上修改

--- hellogit/src/test/HelloGit.java
+++ hellogit/src/test/HelloGit.java
@@ -4,7 +4,8 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello git.....");
+		System.out.println("分支上修改...");
+		System.out.println("hello git 修改.....");
 	}
 
 }
